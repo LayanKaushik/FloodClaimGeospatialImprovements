41108790391934c9de05de143bbdbc201b36eb42
LayanKaushik <layankaushik13@gmail.com>
2024-02-17 19:06:46 -0500

    update working code for fractional regression model, tested with 1000 rows

--- Code/Model_location_info/Fractional_regression_model.R
+++ Code/Model_location_info/Fractional_regression_model.R
@@ -1,4 +1,4 @@
-rm.list()
+rm(list=ls())
 set.seed(420) # for reproducibility
 
 library(lmtest)
@@ -6,14 +6,32 @@ library(sandwich)
 library(arrow)
 library(mgcv)
 library(boot)
+library(dplyr)
+library(glmnet)
+library(ggplot2)
 
 # read this: https://m-clark.github.io/posts/2019-08-20-fractional-regression/
 
-#dataset with encoding
+# read dataset with encoding
 df <- read_parquet('C:/Users/Asus/Box/Flood Damage PredictionProject/Dataset/filtered_dataset_with_geo_info_with_encoding.parquet.gzip')
 
-vars_to_exclude = paste0("svd_", 1:30) # creates svd_1, svd_2, ..., svd_30
-df_modified = df[, !(names(df) %in% vars_to_exclude)]
+# drop all county columns
+columns_to_remove <- grep("countyCode_", names(df), value = TRUE)
+df <- df[, !(names(df) %in% columns_to_remove)]
+
+# drop index column from the dataframe
+if ("__index_level_0__" %in% names(df)) {
+  df <- df[ , !names(df) %in% "__index_level_0__"]
+}
+
+# Randomly sample 10,000 rows from df to test the code
+sampled_indices <- sample(nrow(df), 20000)
+
+sampled_df <- df[sampled_indices, ] #careful when using the entire dataset, use sampled_df instead of df for the code to run
+
+# select geoinfo variables and remove them from df to create the baseline model
+svd_vars = paste0("svd_", 1:30) # creates svd_1, svd_2, ..., svd_30
+df_modified = sampled_df[, !(names(sampled_df) %in% svd_vars)]
 
 # Identifying numeric variables excluding binary and the target variable
 numeric_vars = sapply(df_modified, is.numeric) & !sapply(df_modified, function(x) all(x %in% c(0, 1))) & !(names(df_modified) %in% "buildingrelativeDamage")
@@ -21,81 +39,118 @@ numeric_vars = sapply(df_modified, is.numeric) & !sapply(df_modified, function(x
 # Scaling if required
 df_modified[numeric_vars] = scale(df_modified[numeric_vars])
 
-
-# Define a function to calculate MAE for cross-validation
-mae_func <- function(data, indices) {
-  d_train <- data[indices$train, ]
-  d_test <- data[indices$test, ]
-  
-  # Fit the model using all predictors except 'buildingrelativeDamage'
-  fit <- glm(buildingrelativeDamage ~ ., data=d_train, family=binomial(link="probit")) #can use logit as well
-  
-  se_glm_robust = coeftest(fit, vcov = vcovHC(fit, type="HC3"))
-  
-  # Make predictions on the test set
-  predictions <- predict(se_glm_robust, newdata=d_test, type="response")
-  
-  # Calculate and return MAE
-  actual <- d_test$buildingrelativeDamage
-  mean(abs(predictions - actual))
-}
-
-# Prepare data for cross-validation
-folds <- cv.glm(df_modified, glm(buildingrelativeDamage ~ ., data=df_modified, family=binomial(link="probit")), K=5, cost=mae_func)
-
-# Average MAE on the left out fold
-average_mae <- mean(folds$delta)
-
-#############################
-
-
-svd_vars <- paste0("svd_", 1:30)
-
 # Initialize a vector to store the MAE for baseline and each SVD variable inclusion
-mae_values <- numeric(length(svd_vars) + 1)
+mae_values <- c(Baseline = 0, setNames(numeric(length(svd_vars)), svd_vars))
 
-# Function to calculate MAE
-calc_mae <- function(actual, predicted) {
+# Cost function for cv.glm that calculates MAE
+mae_cost <- function(actual, predicted) {
   mean(abs(actual - predicted))
 }
 
-# Calculate baseline MAE with 5-fold CV excluding all SVD variables
+#Baseline Model
+# Perform 5 fold cross-validation with cv.glm
 baseline_model <- glm(buildingrelativeDamage ~ ., data=df_modified, family=binomial(link="probit"))
-baseline_mae <- cv.glm(df_modified, baseline_model, K=5, cost=calc_mae)
-mae_values[1] <- mean(baseline_mae$delta)
+baseline_mae <- cv.glm(df_modified, baseline_model, K=5, cost=mae_cost)
+
+# The delta contains two elements: the first is the raw cross-validation estimate of prediction error,
+# and the second is the adjusted estimate. For MAE, you'd look at the raw (first) value.
+mae_values["Baseline"] <- baseline_mae$delta[1]
+
+#######################################################################################################
+## check geoinfo column inclusion ##
 
 # Variable selection and iterative cross-validation
 included_vars <- character() # to keep track of included SVD variables
+
 for (i in 1:length(svd_vars)) {
   mae_per_var <- setNames(numeric(length(svd_vars)), svd_vars)
   
   for (svd_var in svd_vars[!svd_vars %in% included_vars]) {
-    temp_df <- cbind(df_modified, df[, svd_var]) # Temporarily include the SVD variable
-    temp_mae <- replicate(30, {
-      folds <- cv.glm(temp_df, glm(buildingrelativeDamage ~ . + included_vars + svd_var, data=temp_df, family=binomial(link="probit")), K=2, cost=calc_mae)
-      mean(folds$delta)
-    })
-    mae_per_var[svd_var] <- mean(temp_mae)
+    
+    temp_df <- cbind(df_modified, sampled_df[, svd_var]) # Temporarily include the SVD variable
+    
+    # Perform cross-validation and calculate MAE
+    cv_result <- cv.glm(temp_df, glm(buildingrelativeDamage ~ ., data=temp_df, family=binomial(link="probit")), K=2, cost=mae_cost)
+    mae_per_var[svd_var] <- mean(cv_result$delta[1])
   }
   
   # Identify the SVD variable with the lowest MAE
   best_svd_var <- names(which.min(mae_per_var))
   included_vars <- c(included_vars, best_svd_var)
   
-  # Rerun 5-fold CV with the newly included SVD variable
-  final_df <- cbind(df_modified, df[, included_vars])
-  final_model_mae <- cv.glm(final_df, glm(buildingrelativeDamage ~ ., data=final_df, family=binomial(link="probit")), K=5, cost=calc_mae)
-  mae_values[i + 1] <- mean(final_model_mae$delta)
+  # Add the best_svd_var to the original dataframe
+  df_modified <- cbind(df_modified, sampled_df[, best_svd_var])
   
-  # Update svd_vars to exclude the newly included variable
+  # Rerun 5-fold CV with the newly included SVD variable
+  final_model_mae <- cv.glm(df_modified, glm(buildingrelativeDamage ~ ., data=df_modified, family=binomial(link="probit")), K=5, cost=mae_cost)
+  mae_values[best_svd_var] <- final_model_mae$delta[1]
+  
   svd_vars <- svd_vars[!svd_vars %in% included_vars]
+  
+  print(mae_values)
+  
 }
 
+## Plot
 
-###########################################################################################################
+# Convert mae_values to a data frame for ggplot
+mae_df <- data.frame(Variable = names(mae_values), MAE = mae_values)
+
+# Plot
+ggplot(mae_df, aes(x = Variable, y = MAE)) +
+  geom_line(aes(group = 1), color = "black", size = 0.5) +
+  geom_point(color = "darkgreen", size = 3) + 
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1),
+        plot.title = element_text(hjust = 0.5)) + 
+  labs(title = "MAE Across Models with SVD Variable Inclusion",
+       x = "SVD Variable Included",
+       y = "Mean Absolute Error (MAE)")
+
+
+
+# save.image(file = paste0("fractional_regression_output_10k_rows", "-", timestamp, ".RData"))
+
+####Fin#####
+################################################################################################################
 
 
 # check here for documentation: https://www.rdocumentation.org/packages/sandwich/versions/3.1-0/topics/vcovHC
+
+# 
+# # Define a function to calculate MAE for cross-validation
+# mae_func <- function(data, indices) {
+#   
+#   d_train <- data[indices$train, ]
+#   d_test <- data[indices$test, ]
+#   
+#   # Fit the model using all predictors except 'buildingrelativeDamage'
+#   fit <- glm(buildingrelativeDamage ~ ., data=d_train, family=binomial(link="probit"))  #can use logit as well
+#   
+#   # Make predictions on the test set
+#   predictions <- predict(fit, newdata=d_test, type="response")
+#   
+#   # Calculate and return MAE
+#   actual <- d_test$buildingrelativeDamage
+#   mean(abs(predictions - actual))
+# }
+# 
+# K <- 5  # Number of folds
+# 
+# # Create indices for K-fold cross-validation
+# folds_indices <- createFolds(df_modified$buildingrelativeDamage, k = K, list = TRUE, returnTrain = TRUE)
+# 
+# # Initialize a vector to store MAE for each fold
+# mae_values <- numeric(K)
+# 
+# # Loop through each fold and calculate MAE
+# for(i in seq_along(folds_indices)) {
+#   indices <- list(train = folds_indices[[i]], test = setdiff(1:nrow(df_modified), folds_indices[[i]]))
+#   mae_values[i] <- mae_func_corrected(df_modified, indices)
+# }
+# 
+# # Calculate average MAE across all folds
+# average_mae <- mean(mae_values)
 
 
 
